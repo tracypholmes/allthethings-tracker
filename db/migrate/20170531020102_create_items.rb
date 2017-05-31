10ac129b45ee6c1bc45a29919edaac44667df697
tracypholmes <tracypholmes@gmail.com>
2017-05-30 21:42:56 -0500

    created migration tables

--- db/migrate/20170531020102_create_items.rb
+++ db/migrate/20170531020102_create_items.rb
@@ -1,4 +1,9 @@
 class CreateItems < ActiveRecord::Migration
   def change
+    create_table :items do |t|
+      t.string :item_type
+      t.string :item_name
+      t.integer :quantity
+    end
   end
 end
--- db/migrate/20170531020112_create_lists.rb
+++ db/migrate/20170531020112_create_lists.rb
@@ -1,4 +1,9 @@
 class CreateLists < ActiveRecord::Migration
   def change
+    create_table :lists do |t|
+      t.string :list_title
+      t.integer :user_id
+      t.integer :item_id
+    end
   end
 end
