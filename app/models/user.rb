6b26485e0f409269a6fba878e89f3770c9d5b7b8
tracypholmes <tracypholmes@gmail.com>
2017-05-30 22:33:32 -0500

    added validations for username, email, pswd, w/ min pswd length

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_many :lists
   has_many :items, through: :lists
   has_secure_password
+
+  # validations for username, email, pswd, w/ min pswd length
+  validates :username, :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
 end
