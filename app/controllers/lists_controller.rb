808bb6fa7a48918cb1b597fec01d7c74e687be77
tracypholmes <tracypholmes@gmail.com>
2017-05-31 23:25:46 -0500

    added list and list creation w/login redirect to listcontroller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,20 @@
 class ListsController < ApplicationController
 
-end
+  get '/lists' do
+    if logged_in?
+      @lists = List.all
+      erb :'lists/lists'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/lists/new' do
+    if logged_in?
+      erb :'/lists/create_list'
+    else
+      redirect to '/login'
+    end
+  end
+
+  
