16971586243439c4b4118784cd5883e99ec52ed3
tracypholmes <tracypholmes@gmail.com>
2017-05-31 23:31:59 -0500

    show & find list to lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,4 +17,23 @@ class ListsController < ApplicationController
     end
   end
 
+  post '/lists' do
+    if params.value?('')
+      redirect to '/lists/new'
+    else
+      user = User.find(session[:user_id])
+      List.create(user_id: user.id, list_title: params[:list_title])
+      redirect to '/lists'
+    end
+  end
+
+  get '/lists/:id' do
+    if logged_in?
+      @list = List.find_by_id(params[:id])
+      erb :'/lists/show_list'
+    else
+      redirect to '/login'
+    end
+  end
+
   
