c74f54b397ccc0650d054ae6a419202aa00ec670
tracypholmes <tracypholmes@gmail.com>
2017-06-14 14:52:10 -0500

    dropped db; remigrating

--- db/migrate/20170614032422_create_lists.rb
+++
@@ -1,8 +0,0 @@
-class CreateLists < ActiveRecord::Migration
-  def change
-    create_table :lists do |t|
-      t.string :list_title
-      t.integer :user_id
-    end
-  end
-end
--- db/migrate/20170614035917_add_user_id_to_item.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToItem < ActiveRecord::Migration
-  def change
-    add_column :items, :user_id, :integer
-  end
-end
