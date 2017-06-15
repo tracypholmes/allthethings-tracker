e8b654af09b0cd1dac83d68525deea1ea3012110
tracypholmes <tracypholmes@gmail.com>
2017-06-15 10:57:36 -0500

    updated list references > comics & flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,27 @@
 class UsersController < ApplicationController
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
-    erb :'/users/user_lists'
+    erb :'/users/user_comics'
   end
 
   get '/signup' do
     if !logged_in?
       erb :'/users/create_user'
     else
-      redirect to '/lists'
+      redirect to '/comics'
     end
   end
 
   post '/signup' do
     user = User.create(params) # have them create a new account
-    # if user saves assign session id to user id redirect them to the lists
+    # if user saves assign session id to user id redirect them to the comics
     if user.save
       flash[:notice] = "Thanks for signing up!"
       session[:id] = user.id
-      redirect to '/lists'
+      redirect to '/comics'
     else # otherwise
+      # added for validation error messages
+      flash[:error] = user.errors.full_messages.join
       redirect to '/signup' # redirect them to signup
     end
   end
@@ -28,21 +30,29 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'/users/login'
     else
-      redirect '/lists'
+      redirect '/comics'
     end
   end
 
   post '/login' do
     user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect '/lists'
+    if user
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect '/comics'
+      elsif
+        flash[:error] = "Invalid password"
+        redirect to '/login'
+      else
+        flash[:error] = "Invalid username"
+        redirect to '/login'
+      end
     else
-      flash.now[:notice] = "This account does not exist. Please create one."
+      flash.now[:error] = "This account does not exist. Please create one."
       redirect to '/signup'
     end
   end
-
+  
   get '/logout' do
     if logged_in?
       session.destroy
