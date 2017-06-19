6c37eff7cc6419a56ac9b2075bde8a7f7733b6b8
tracypholmes <tracypholmes@gmail.com>
2017-06-18 20:50:55 -0500

    refactored edit and /comics routes

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -18,13 +18,12 @@ class ComicsController < ApplicationController
   end
 
   post '/comics' do
-
-    if params.value?('')
-      redirect to '/comics/new'
-    else
-      user = User.find(session[:user_id])
-      Comic.create(user_id: user.id, title: params[:title], series_name: params[:series_name], creator: params[:creator], publisher: params[:publisher], published_date: params[:published_date], issue: params[:issue], media_type: params[:media_type])
+  comic = current_user.comics.build(params[:comic])
+    if comic.save
       redirect to '/comics'
+    else
+      flash[:notice] = comic.errors.full_messages.uniq.join(',')
+      erb :'/comics/add_comic'
     end
   end
 
@@ -39,8 +38,8 @@ class ComicsController < ApplicationController
 
   get '/comics/:id/edit' do
     if logged_in?
-      @comic = Comic.find_by_id(params[:id])
-      if @comic.user_id == current_user.id
+      @comic = current_user.comics.find_by(id: params[:id])
+      if @comic
         erb :'/comics/edit_comic'
       else
         flash[:notice] = 'You can only edit your own comic books!'
