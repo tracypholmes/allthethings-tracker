46ee5fa075fde902145d22af9abe1a4358b0f68e
tracypholmes <tracypholmes@gmail.com>
2017-06-18 20:18:06 -0500

    Took out user redundancy for authentication , corrected id to user_id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     user = User.new(params) 
     if user.save
       flash[:notice] = "Thanks for signing up!"
-      session[:id] = user.id
+      session[:user_id] = user.id
       redirect to '/comics'
     else 
       flash[:notice] = user.errors.full_messages.join
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   post '/login' do
     user = User.find_by(username: params[:username])
     if user
-      if user && user.authenticate(params[:password])
+      if user.authenticate(params[:password])
         session[:user_id] = user.id
         redirect '/comics'
       else
